42f794b16faa6c41ce4f39574f16be9a958d7454
Rizwanelansyah <rizwanelansyah55@gmail.com>
2023-11-06 13:11:16 +0700

    115

---
+++ no115AccessAllClassInPackage/Main.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+import java.util.Random
+
+fun main() {
+  val rand = Random()
+  val input = Scanner(System.`in`)
+
+  print("NOMOR ACAK\n")
+  print("Masukkan Nilai MIN :")
+  val nilaiAwal = input.nextInt()
+  print("Masukkan Nilai MAX :")
+  val nilaiAkhir = input.nextInt()
+
+  val randomNumber = rand.nextInt(nilaiAkhir - nilaiAwal + 1) + nilaiAwal
+  println("Nilai Acak : $randomNumber") 
+}
